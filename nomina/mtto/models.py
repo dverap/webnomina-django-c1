3ff40bd34cc35a5e8189a9ab178fda92582cd6b8
dverap <dverap@unemi.edu.ec>
2022-03-10 12:41:55 -0500

    v5

--- nomina/mtto/models.py
+++ nomina/mtto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Cargo(models.Model):
+    descripcion = models.CharField(max_length=100,blank=True,null=True)
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=False,auto_now=True)
+
+class Departamento(models.Model):
+    descripcion = models.CharField(max_length=100,blank=True,null=True)
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=False,auto_now=True)
+
